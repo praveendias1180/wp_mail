e2504964bcdd65dafa405763c480ba704debe0f7
Praveen Dias <praveendias1180@gmail.com>
2022-11-02 18:26:07 +0530

    Added example configuration

---
+++ mail-config-sample.php
@@ -0,0 +1,6 @@
+<?php
+define("P1180_MAIL_TO", 'yourmail@gmail.com');
+
+define("P1180_SMTP", 'smtp.gmail.com');
+define("P1180_MAIL_FROM", 'yourmailfrom@gmail.com');
+define("P1180_APP_PASSWORD", 'yourapppassword');
